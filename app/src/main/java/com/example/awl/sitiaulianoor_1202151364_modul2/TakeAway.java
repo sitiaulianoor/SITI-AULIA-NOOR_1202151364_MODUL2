1cac054f5d21a914465cf3dda3afc2e5b38cd2ff
sitiaulianoor <sitiaulianoor@gmail.com>
2018-02-18 16:53:40 +0700

    Third Commit

--- app/src/main/java/com/example/awl/sitiaulianoor_1202151364_modul2/TakeAway.java
+++ app/src/main/java/com/example/awl/sitiaulianoor_1202151364_modul2/TakeAway.java
@@ -23,44 +23,6 @@ public class TakeAway extends AppCompatActivity  {
         setContentView(R.layout.activity_take_away);
     }
 
-   //public void showDatePickerDialog(View view) {
-     //   DialogFragment newFragment = new DatePickerFragment();
-       // newFragment.show(getSupportFragmentManager(), getString(R.string.date_picker));--!>
-
- //   }
-  //  public void processDatePickerResult(int year, int month, int day) {
-    //    String month_string = Integer.toString(month + 1);
-      //  String day_string = Integer.toString(day);
-       // String year_string = Integer.toString(year);
-        // Assign the concatenated strings to dateMessage.
-        //String dateMessage = (month_string + "/" +
-        //        day_string + "/" + year_string);
-      //  EditText eTanggal = (EditText) findViewById(R.id.eTanggal);
-       // eTanggal.setText(dateMessage);
-
-        //Toast.makeText(this, dateMessage,
-          //      Toast.LENGTH_SHORT).show();
- //   }
-
-  //  public void showTimePickerDialog(View view) {
-    //    DialogFragment newFragment = new TimePickerFragment();
-      //  newFragment.show(getSupportFragmentManager(),
-        //        getString(R.string.time_picker));
-
-    //}
-
-    //public void processTimePickerResult(int hourOfDay, int minute) {
-        // Convert time elements into strings.
-      //  String hour_string = Integer.toString(hourOfDay);
-        //String minute_string = Integer.toString(minute);
-        // Assign the concatenated strings to timeMessage.
-
-        //EditText eWaktu = (EditText) findViewById(R.id.eWaktu);
-        //String timeMessage = (hour_string + ":" + minute_string);
-       // eWaktu.setText(timeMessage);
-        //Toast.makeText(this, timeMessage,
-          //      Toast.LENGTH_SHORT).show();
-   // }
 
     public void PesanTakeAway(View view) {
         Intent i = new Intent(this, ContentMenu.class);
